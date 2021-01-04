a2882d1e89e4fdb8eb4fc26640d1632b987727cb
Lilian <lilianouvmmi@gmail.com>
2021-01-04 17:03:18 +0100

    add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Ingredient.count == 0
+    path = File.join(File.dirname(__FILE__), "./data/ListIngredients.json")
+    records = JSON.parse(File.read(path))
+    records.each do |record|
+        Ingredient.create!(record)
+    end
+    puts "Ingredient are seeded"
+end
+
+if RecipeIngredient.count == 0
+    path = File.join(File.dirname(__FILE__), "./data/ListRecipeIngredients.json")
+    records = JSON.parse(File.read(path))
+    records.each do |record|
+        RecipeIngredient.create!(record)
+    end
+    puts "RecipeIngredient are seeded"
+end
