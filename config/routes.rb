be4ef4b7cd13c899f1e34a1f10cfd0692ebd5144
MaelFournier <mfournier2@la-joliverie.com>
2021-01-11 16:09:51 +0100

    add signin functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'user/index'
   get 'user/show'
   get 'user/new'
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   get 'inscription', to: 'inscription#index'
+  get 'sessions/create'
+  get 'sessions/error'
+  get 'sessions/destroy'
+
 
 end
