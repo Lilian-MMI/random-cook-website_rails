84c909ae3df77da443838f006f34d5f8d623ac1c
marth154 <37938187+marth154@users.noreply.github.com>
2021-01-12 15:35:13 +0100

    modif pres

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
     has_many :favorites
     has_many :recipe_ingredients
     has_many :steps
+    belongs_to :user, optional: true
 
     accepts_nested_attributes_for :recipe_ingredients, :steps, :favorites
 end
